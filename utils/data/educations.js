65dd7d67b9b38864a6259b8a96a1be611c814e1c
ThomsMTZ <45689292+ThomsMTZ@users.noreply.github.com>
2025-08-07 15:25:05 +0300

    Based project
    Adding GTM token

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Engineering Degree in Computer Science & Technology ",
+    duration: "2017 – 2020",
+    institution: "PolyTech School in Marseille",
+  },
+  {
+    id: 2,
+    title: "TOEIC",
+    duration: "2020",
+    institution: "835 points",
+  },
+  {
+    id: 3,
+    title: "PSM 1 Scrum Master",
+    duration: "2024",
+    institution: "Scrum.org",
+  }
+]
